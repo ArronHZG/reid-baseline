ce5027bd1a69c90527607491a25fa38df6dd5f20
ArronHZG <hou.zg@foxmail.com>
2020-05-24 02:09:46 +0800

    05-23

--- script/05-21-2020.sh
+++ script/05-21-2020.sh
@@ -1,22 +1,8 @@
-python ../tools/train.py --config_file='../configs/joint.yml' \
-MODEL.IF_IBN_A True \
-TRAIN.MAX_EPOCHS 300 \
-MODEL.DEVICE_ID  1
+python ../tools/train.py --config_file='../configs/joint.yml'
 
 python ../tools/train.py --config_file='../configs/joint.yml' \
-MODEL.IF_IBN_A True \
-TRAIN.MAX_EPOCHS 300 \
-LOSS.IF_LEARNING_WEIGHT = True \
-MODEL.DEVICE_ID  1
+LOSS.IF_LEARNING_WEIGHT = True
 
+python ../tools/train.py --config_file='../configs/msmt17.yml'
 
-python ../tools/train.py --config_file='../configs/msmt17.yml' \
-MODEL.IF_IBN_A True \
-TRAIN.MAX_EPOCHS 300 \
-MODEL.DEVICE_ID  1
-
-
-python ../tools/train.py --config_file='../configs/dukemtmc.yml' \
-MODEL.IF_IBN_A True \
-TRAIN.MAX_EPOCHS 300 \
-MODEL.DEVICE_ID  1
+python ../tools/train.py --config_file='../configs/dukemtmc.yml'
--- script/05-23-2020.sh
+++ script/05-23-2020.sh
@@ -2,8 +2,10 @@ python ../tools/train.py --config_file='../configs/autoencoder.yml'
 
 python ../tools/train.py --config_file='../configs/autoencoder.yml' \
 EBLL.CODE_SIZE 512
+
 python ../tools/train.py --config_file='../configs/autoencoder.yml' \
 EBLL.LAMBDA 0.1
+
 python ../tools/train.py --config_file='../configs/autoencoder.yml' \
 EBLL.LAMBDA 0.05
 
